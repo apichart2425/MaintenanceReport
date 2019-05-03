88123c6fefd807909d0a6d1bd93870c6f3ef41d9
apichart2425 <60070111@kmitl.ac.th>
2019-05-03 14:47:50 +0700

    start login page

--- MaintenanceReport/MaintenanceReport/urls.py
+++ MaintenanceReport/MaintenanceReport/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reports/', include('reports.urls'))
+    path('', include('reports.urls')),
 ]
--- MaintenanceReport/reports/urls.py
+++ MaintenanceReport/reports/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('', views.my_login, name='login'),
     path('register/', views.my_register, name='register'),
     path('reportform/', views.report_form, name='reportform'),
-    path('login/', views.my_login, name='login'),
     path('detail/<int:maintenance_id>', views.detail, name='detail'),
     path('logout/', views.my_logout, name='logout')
 ]
