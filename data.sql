a92b94f7ca3bbc8c0a074f6c31e1f11140e34058
pumy2517 <60070067@kmitl.ac.th>
2019-05-08 10:07:13 +0700

    sql

--- data.sql
+++ data.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `webproject` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `webproject`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: localhost    Database: webproject
@@ -602,4 +604,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-08 10:03:49
+-- Dump completed on 2019-05-08 10:06:39
